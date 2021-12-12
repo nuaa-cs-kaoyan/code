4e0fae21d539f3654e4ccf8bbbf4f87cdb8a3969
wansho <wanshojs@hotmail.com>
2021-12-12 15:03:33 +0800

    update

--- 真题代码/2021-堆排序.c
+++ 真题代码/2021-堆排序.c
@@ -18,7 +18,9 @@ int* adjust(int A[], int index, int value, int length) {
 	A[index] = value;
 	int parentIndex = index / 2;
 	int leftChildIndex = index * 2;
-	
+    int rightChildIndex = leftChildIndex + 1;
+    int minChildIndex;
+    // 上溯
 	while(index != 1 && A[parentIndex] > A[index]){
 		A[0] = A[parentIndex];
 		A[parentIndex] = A[index];
@@ -26,13 +28,18 @@ int* adjust(int A[], int index, int value, int length) {
 		index = parentIndex;
 		parentIndex = index / 2;
 	}
-	
-	while(leftChildIndex <= length && A[index] > A[leftChildIndex]){
-		A[0] = A[index];
-		A[index] = A[leftChildIndex];
-		A[leftChildIndex] = A[0];
-		index = leftChildIndex;
+    // 下溯
+	while((leftChildIndex <= length && A[index] > A[leftChildIndex]) ||
+          (leftChildIndex <= length && A[index] > A[rightChildIndex])){ // 存在子节点小于父节点
+        if(rightChildIndex <= length && A[leftChildIndex] > A[rightChildIndex]){ // 取最小的子节点
+        	minChildIndex = rightChildIndex;
+        }  
+		A[0] = A[minChildIndex];
+		A[minChildIndex] = A[index];
+		A[index] = A[0];
+		index = minChildIndex;
 		leftChildIndex = leftChildIndex * 2;
+        rightChildIndex = leftChildIndex + 1;
 	}
 	return A;
 }
@@ -41,7 +48,7 @@ int* adjust(int A[], int index, int value, int length) {
 int main(void){
 	int A[11] = {0, 53, 87, 161, 108, 462, 503, 175, 170, 897, 512};
 //	*A = adjust(A, 2, 1000, 10);
-	*A = adjust(A, 10, 88, 10);	
+	*A = adjust(A, 3, 176, 10);	
 	int i = 1;
 	while(i < 11){
 		printf("%d \n", A[i]);
